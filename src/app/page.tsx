3c1ef618abe3fafcdf8018bc78935b208c031ada
Victor Bodell <vbodell@kth.se>
2024-04-08 21:23:05 +0200

    Update styling and move out data source from code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@
 import { useState, useEffect } from "react";
 import classNames from "classnames";
 
+import numbers from "./numbers.json";
+
 interface GameProps {
   responseChoices: ResponseChoice[];
   gameDurationInSec: number;
@@ -52,6 +54,12 @@ function GamePresenter({ responseChoices, gameDurationInSec }: GameProps) {
   const randomIndex = Math.floor(Math.random() * randomOpts.length);
   const question = randomOpts[randomIndex].id;
 
+  function resetGame() {
+      setGameOver(false);
+      setScore(0);
+      setAnsweredQuestions(0);
+  }
+
   function answerSubmitted(wasCorrect: boolean, clearAnswer: () => void) {
     setTimeout(() => {
       setAnsweredQuestions(answeredQuestions + 1);
@@ -66,7 +74,7 @@ function GamePresenter({ responseChoices, gameDurationInSec }: GameProps) {
         {score} / {answeredQuestions}
       </p>
       {gameOver ? (
-        <button onClick={() => setGameOver(false)}>
+        <button className="rounded-lg p-4 bg-purple-700" onClick={resetGame}>
           Go again! ({gameDurationInSec}s)
         </button>
       ) : (
@@ -83,7 +91,7 @@ function GamePresenter({ responseChoices, gameDurationInSec }: GameProps) {
 function QuestionView({ question, choices, choiceLayout }: QuestionViewProps) {
   return (
     <>
-      <div>{question}</div>
+      <div className="text-3xl">{question}</div>
       <div className={choiceLayout}>
         {choices.map((choice) => (
           <button
@@ -125,13 +133,14 @@ function QuestionPresenter({
     text: text,
     onClick: () => submitResponse(id),
     className: classNames(
-      "border border-white rounded",
+      "rounded",
+      "p-2",
       hasAnswered && isCorrectAnswer(id)
-        ? "bg-green-100"
+        ? "bg-green-700"
         : isSelectedAnswer(id)
-          ? "bg-red-100"
-          : "bg-blue-100",
-      !hasAnswered && "bg-blue-100",
+          ? "bg-red-700"
+          : "bg-purple-700",
+      !hasAnswered && "bg-purple-700",
     ),
   }));
   const choiceLayout = "grid grid-cols-3 grid-rows-3 gap-2";
@@ -146,282 +155,7 @@ function QuestionPresenter({
 }
 
 export default function Home() {
-  const opts = [
-    { id: "00", person: "Alvina" },
-    { id: "01", person: "Frodo" },
-    { id: "02", person: "Churchill" },
-    { id: "03", person: "Gustav L" },
-    { id: "04", person: "Pontus" },
-    { id: "05", person: "Kung David" },
-    { id: "06", person: "Gollum" },
-    { id: "07", person: "Bond" },
-    { id: "08", person: "Sherlock" },
-    { id: "09", person: "Gandalf" },
-    { id: "10", person: "Millimeter" },
-    { id: "11", person: "Billy" },
-    { id: "12", person: "London (Petrus)" },
-    { id: "13", person: "Spartacus (Paulus) " },
-    { id: "14", person: "Gimli" },
-    { id: "15", person: "Biff" },
-    { id: "16", person: "Qui-Gon" },
-    { id: "17", person: "Veronica Maggio" },
-    { id: "18", person: "Carl Bodell" },
-    { id: "19", person: "Tim Roth (1900)" },
-    { id: "20", person: "Roger" },
-    { id: "21", person: "Kevin Spacey" },
-    { id: "22", person: "Farfar" },
-    { id: "23", person: "Donkey Kong" },
-    { id: "24", person: "Kalle Anka" },
-    { id: "25", person: "Obama" },
-    { id: "26", person: "Michael League" },
-    { id: "27", person: "Superman" },
-    { id: "28", person: "Lisbeth Salander" },
-    { id: "29", person: "Roy" },
-    { id: "30", person: "Ante" },
-    { id: "31", person: "Joey" },
-    { id: "32", person: "Richard Page" },
-    { id: "33", person: "J.D." },
-    { id: "34", person: "Super Mario" },
-    { id: "35", person: "Emil i Lönneberga" },
-    { id: "36", person: "Einstein" },
-    { id: "37", person: "Tuna" },
-    { id: "38", person: "Yoshi" },
-    { id: "39", person: "Dyrsén" },
-    { id: "40", person: "House" },
-    { id: "41", person: "Kapten Jörgen" },
-    { id: "42", person: "Marvin (roboten)" },
-    { id: "43", person: "Frank Sinatra" },
-    {
-      id: "44",
-      person: "William Bodell",
-    },
-    {
-      id: "45",
-      person: "Hitler",
-    },
-    {
-      id: "46",
-      person: "Märta",
-    },
-    {
-      id: "47",
-      person: "GW",
-    },
-    {
-      id: "48",
-      person: "Maya-indian",
-    },
-    {
-      id: "49",
-      person: "Askungen",
-    },
-    {
-      id: "50",
-      person: "Hans Andréasson",
-    },
-    {
-      id: "51",
-      person: "Dustin Hoffman (rainman)",
-    },
-    {
-      id: "52",
-      person: "Forrest Gump",
-    },
-    {
-      id: "53",
-      person: "Zlatan",
-    },
-    {
-      id: "54",
-      person: "Legolas",
-    },
-    {
-      id: "55",
-      person: "Doc Emmet L Brown",
-    },
-    {
-      id: "56",
-      person: "Törnrosa",
-    },
-    {
-      id: "57",
-      person: "Fagen",
-    },
-    {
-      id: "58",
-      person: "Farmor Hjördis",
-    },
-    {
-      id: "59",
-      person: "Miles Davis",
-    },
-    {
-      id: "60",
-      person: "Mamma",
-    },
-    {
-      id: "61",
-      person: "Sam Winchester",
-    },
-    {
-      id: "62",
-      person: "Pappa",
-    },
-    {
-      id: "63",
-      person: "Bobby",
-    },
-    {
-      id: "64",
-      person: "Link",
-    },
-    {
-      id: "65",
-      person: "Abraham Lincoln",
-    },
-    {
-      id: "66",
-      person: "Justin Timberlake",
-    },
-    {
-      id: "67",
-      person: "Dean Winchester",
-    },
-    {
-      id: "68",
-      person: "Erik Jonasson",
-    },
-    {
-      id: "69",
-      person: "Austin Powers",
-    },
-    {
-      id: "70",
-      person: "Johannes av korset",
-    },
-    {
-      id: "71",
-      person: "Henke Lundqvist",
-    },
-    {
-      id: "72",
-      person: "Jim Carrey",
-    },
-    {
-      id: "73",
-      person: "Vanheden",
-    },
-    {
-      id: "74",
-      person: "Chandler Bing",
-    },
-    {
-      id: "75",
-      person: "Sickan",
-    },
-    {
-      id: "76",
-      person: "Castiel",
-    },
-    {
-      id: "77",
-      person: "Jesus",
-    },
-    {
-      id: "78",
-      person: "John Travolta",
-    },
-    {
-      id: "79",
-      person: "Karlsson på taket",
-    },
-    {
-      id: "80",
-      person: "Sting",
-    },
-    {
-      id: "81",
-      person: "Leonardo DiCaprio",
-    },
-    {
-      id: "82",
-      person: "Barney Stinson",
-    },
-    {
-      id: "83",
-      person: "MJ",
-    },
-    {
-      id: "84",
-      person: "Mormor Iggy",
-    },
-    {
-      id: "84",
-      person: "Mormor Iggy",
-    },
-    {
-      id: "85",
-      person: "Marty McFly",
-    },
-    {
-      id: "86",
-      person: "Elwood Blues",
-    },
-    {
-      id: "87",
-      person: "Whitney Houston",
-    },
-    {
-      id: "88",
-      person: "Claes Eriksson",
-    },
-    {
-      id: "89",
-      person: "Oscar Bodell",
-    },
-    {
-      id: "90",
-      person: "DaVinci",
-    },
-    {
-      id: "91",
-      person: "Sofie Sundell",
-    },
-    {
-      id: "92",
-      person: "Anders Götvall",
-    },
-    {
-      id: "93",
-      person: "Jag",
-    },
-    {
-      id: "94",
-      person: "Hagrid",
-    },
-    {
-      id: "95",
-      person: "Ch",
-    },
-    {
-      id: "96",
-      person: "Hermione",
-    },
-    {
-      id: "97",
-      person: "Harry Potter",
-    },
-    {
-      id: "98",
-      person: "CS Lewis",
-    },
-    {
-      id: "99",
-      person: "David Paich",
-    },
-  ];
-
-  const responseChoices: ResponseChoice[] = opts.map(({ id, person }) => ({
+  const responseChoices: ResponseChoice[] = numbers.map(({ id, person }) => ({
     id: id,
     text: person,
   }));
@@ -437,7 +171,7 @@ export default function Home() {
           gameDurationInSec={GAME_DURATION}
         />
       ) : (
-        <button onClick={() => setStarted(true)}>
+        <button className="rounded-lg p-4 bg-purple-700" onClick={() => setStarted(true)}>
           Let's Go! ({GAME_DURATION}s)
         </button>
       )}
