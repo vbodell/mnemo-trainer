672ea38a6bcc97a5ce1c5b1ce0b72482ad178cf8
Victor Bodell <victor.bodell@sr.se>
2024-04-09 23:50:57 +0200

    Diff tests work!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useState } from "react";
 import numbers from "../data/numbers.json";
 import { ResponseChoice } from "./Presenters/Game";
 import GamePresenter from "./Presenters/Game";
+import ReactDiffViewer from "react-diff-viewer-continued";
 
 type GameChoice =
   | "number2person"
@@ -72,36 +73,62 @@ export default function Home() {
   const [started, setStarted] = useState<boolean>(false);
   const GAME_DURATION = 60;
 
+  const oldCode = "In a hole in the ground there lived a hobbit";
+  const [newCode, setNewCode] = useState<string>("");
+
+  function handleSubmit(e) {
+    // Prevent the browser from reloading the page
+    e.preventDefault();
+
+    // Read the form data
+    const form = e.target;
+    const formData = new FormData(form);
+
+    // Or you can work with it as a plain object:
+    const formJson = Object.fromEntries(formData.entries());
+    console.log(formJson);
+    setNewCode(formJson["input"].toString());
+    setStarted(true);
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       {started ? (
-        <GamePresenter
-          responseChoices={responseChoices}
-          gameDurationInSec={GAME_DURATION}
+        // <GamePresenter
+        //   responseChoices={responseChoices}
+        //   gameDurationInSec={GAME_DURATION}
+        // />
+        <ReactDiffViewer
+          oldValue={oldCode}
+          newValue={newCode}
+          splitView={true}
         />
       ) : (
-        <form className="flex flex-col gap-5">
-          <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-            Pick a category
-            <select
-              value={gameChoice}
-              onChange={(e) => setGameChoice(e.target.value as GameChoice)}
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            >
-              {Object.entries(GAME_CHOICES).map(([key, val]) => (
-                <option key={key} value={key}>
-                  {val}
-                </option>
-              ))}
-            </select>
-          </label>
-
-          <button
-            className="rounded-lg p-4 bg-purple-700"
-            onClick={() => setStarted(true)}
-          >
-            Let{"'"}s Go! ({GAME_DURATION}s)
-          </button>
+        // <form className="flex flex-col gap-5">
+        //   <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+        //     Pick a category
+        //     <select
+        //       value={gameChoice}
+        //       onChange={(e) => setGameChoice(e.target.value as GameChoice)}
+        //       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        //     >
+        //       {Object.entries(GAME_CHOICES).map(([key, val]) => (
+        //         <option key={key} value={key}>
+        //           {val}
+        //         </option>
+        //       ))}
+        //     </select>
+        //   </label>
+        //
+        //   <button
+        //     className="rounded-lg p-4 bg-purple-700"
+        //     onClick={() => setStarted(true)}
+        //   >
+        //     Let{"'"}s Go! ({GAME_DURATION}s)
+        //   </button>
+        <form method="post" onSubmit={handleSubmit}>
+          <textarea name="input" />
+          <button>Submit</button>
         </form>
       )}
     </main>
